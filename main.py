51aa5959c9f1e72a067fbc7d9f51c99d26336b3b
Lorddeepwhite <45059291+Lorddeepwhite@users.noreply.github.com>
2022-08-04 17:55:52 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 from flask import Flask
+import sys
+import os.path
+from subprocess import call
 
+d = os.path.dirname(os.path.realpath(__file__))
 app = Flask(__name__)
 
 @app.route("/firebase")
 def firebase_call():
-  import firebase.slice_firebase_version
-  import firebase.snortparser_firebase
-  
+  call(["python3", f"{d}/Firebase/slice_firebase_version.py"])
+  call(["python3", f"{d}/Firebase/snortparser_firebase.py"])
+
 @app.route("/mariadb")
-def mariadb_call():
-  import MariaDB.slice
-  import MariaDB.snortparser
-  
+def MariaDB_call():
+  call(["python3", f"{d}/MariaDB/slice.py"])
+  call(["python3", f"{d}/MariaDB/snortparser.py"])
+
 @app.route("/terminal")
 def terminal_call():
-  import databaseterminal
-  import database_terminal_firebase
+  call(["python3", f"{d}/databaseterminal.py"])
+  call(["python3", f"{d}/database_terminal_firebase.py"])
