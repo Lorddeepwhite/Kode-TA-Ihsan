2210a281eb2aa9b2a54b842f318448a3b068d7d4
dreadedhairlock <45059313+dreadedhairlock@users.noreply.github.com>
2022-08-08 17:09:01 +0700

    Update slice_firebase_version.py

--- Firebase/slice_firebase_version.py
+++ Firebase/slice_firebase_version.py
@@ -135,7 +135,7 @@ if not my_file.is_file():
 from_line = 0
 with open(linecountpath, "r") as f :
     from_line = int(f.readline())
-filepath1 = '/Kode_TA_Ihsan/ejbca1.log'
+filepath1 = 'ejbca1.log'
 
 
 if isfile(filepath1):
