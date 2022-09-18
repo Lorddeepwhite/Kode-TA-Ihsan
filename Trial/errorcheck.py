7d2b4a8a856d35726e3657ee99ed912fc54fd178
Lorddeepwhite <45059291+Lorddeepwhite@users.noreply.github.com>
2022-09-18 16:00:40 +0700

    Delete Trial directory

--- Trial/errorcheck.py
+++
@@ -1,8 +0,0 @@
-dataLog = []
-with open('/home/ihsanfr/Downloads/ejbca1.log', 'r') as f:
-    data = f.readlines()
-for line in data:
-    if 'INFO' in line:
-        print(line)
-        dataLog.append(line)
-print(dataLog)
